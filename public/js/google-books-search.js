f0eeceab70baca4391008fb14bc8216392cdc416
Eric Furspan <eric.furspan@gmail.com>
2018-04-15 22:42:58 -0400

    finished work on mvp mock data branch

--- public/js/google-books-search.js
+++ public/js/google-books-search.js
@@ -2,32 +2,31 @@ const GOOGLE_API_KEY = "AIzaSyAOmOmwmXQXzBOvc2Cu7kEjZIkZMwvXPBQ";
 
 function searchAllVolumes(searchString, callback) {
     $.ajax( {
-        url: `https://www.googleapis.com/books/v1/volumes?q=${searchString}&key=${GOOGLE_API_KEY}`,
+        url: `https://www.googleapis.com/books/v1/volumes?q=${searchString}&projection=lite&key=${GOOGLE_API_KEY}`,
         dataType: 'json', 
         data: {
             maxResults: 40
         }
     })
     .done(function( data ) {
-        console.log(data);
         callback(data);
     })
     .fail(function( err) {
-        console.log(err);
+        console.error(err);
     })
 }
 
-function searchSingleVolume(id) {
+function searchSingleVolume(id, callback) {
     $.ajax( {
         url: `https://www.googleapis.com/books/v1/volumes/${id}?key=${GOOGLE_API_KEY}`,
         dataType: 'json', 
         data: {}
     })
     .done(function( data ) {
-        console.log(data);
+        callback(data);
     })
     .fail(function (err) {
-        console.log(err);
+        console.error(err);
     })
 };
 
