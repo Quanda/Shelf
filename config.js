0ffc1043c77021acaa2d09106cdb48d6c23aeba8
Eric Furspan <eric.furspan@gmail.com>
2018-05-08 12:43:07 -0400

    debugging mongodb connection issue

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017 /shelf'; // 127.0.0.1:27017 or 'mongodb://localhost/shelf'
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://127.0.0.1:27017/test-shelf';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/shelf'; // localhost or 127.0.0.1
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/test-shelf';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'badatheetathaytheeban12345';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
--- public/js/login.js
+++ public/js/login.js
@@ -19,24 +19,4 @@ $('.proceed-form').submit(function(event) {
     window.location.replace("/home.html");
 })
 
-// calls login API route to authenticate
-function authenticateUser() {
-    $.ajax({
-       url: '/api/auth/login',
-       type: 'POST',
-       data: {username: $('#username').val(), password: $('#password').val()},
-       dataType: 'JSON'
-    })
-    .done(function( data ) {
-        sessionStorage.setItem('token', data.token);
-        $('.auth-warning').removeClass('warning-on').text('');
-        $('.proceed-btn').removeClass('hidden');
-        $('.login-btn').addClass('hidden');
-    })
-    .fail(function (err) {
-        console.log(err);
-        $('.proceed-btn').addClass('hidden');
-        $('.auth-warning').addClass('warning-on').text(`${err.responseText}`);
-    })
-}
 
