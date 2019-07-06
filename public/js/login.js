c50e0240b1b4f3cbecdbc416c1a09c57dffe2d68
Eric Furspan <eric.furspan@gmail.com>
2019-07-06 11:54:27 -0400

    Remove UI

--- public/js/login.js
+++
@@ -1,9 +0,0 @@
-
-// authenticate user after login form is submitted
-$('.login-form').submit(function(event) {
-    event.preventDefault();
-        
-    // get and set jwt to sessionStorage
-    // redirects to home page if successful
-    authenticateUser();
-});
