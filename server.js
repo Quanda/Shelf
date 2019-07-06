b20e4462ce19fd5d79e511ce7fc63a852e188680
Eric Furspan <eric.furspan@gmail.com>
2019-07-06 13:47:39 -0400

    Test cors fix

--- server.js
+++ server.js
@@ -28,8 +28,9 @@ app.use(morgan('common'));
 // CORS
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With, Origin');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, Authorization, Content-Type, Accept, X-Requested-With');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
   }
